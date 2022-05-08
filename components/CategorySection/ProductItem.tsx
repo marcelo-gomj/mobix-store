6fd2deb5fef26c2dbab7d659fddee3c7ce224959
marcelo-gomj <marcelomgj005@gmail.com>
2022-05-08 16:12:02 -0300

    fix: products card interface

--- components/CategorySection/ProductItem.tsx
+++ components/CategorySection/ProductItem.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 
 import { ProductsProps } from '../../pages';
+import Fav from '../../Icons/fav.svg';
 
 import item from './ProductItem.module.css';
 
@@ -13,21 +14,48 @@ export function ProductsItem({ product } : ProductsItemProps){
         return Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(value)
     }
 
-    console.log(product);
+    function reduceTitleProduct(title : string){
+        return title.split('')
+            .reduce((acc, word, index) => {
+                if(index < 60){
+                    return acc + word
+                }
+
+                if(index === 60) return acc + '...'
+
+                return acc
+            }, '');
+    }
+
     return (
         <li className={item.card}>
-            
-            <Image 
-                src={product.image} 
-                alt={product.title}
-                height='100px'
-                width='60px'
-                quality='100'
-            />
+            <div className={item.product_image}>
+                <Image 
+                    src={product.image} 
+                    alt={product.title}
+                    layout='fill'
+                    quality='100'
+                />
+            </div>
 
-            <h3>{product.title}</h3>
-            <p>{formatPrice(product.price)}</p>
-            <span>{product.rating.rate}</span>
+            <div className={item.description}>
+                <h3>{reduceTitleProduct(product.title)}</h3>
+                
+                <footer>
+                    <p className={item.price}>{formatPrice(product.price)}</p>
+                    <div className={item.extra_infos}>
+                       <div className={item.classification}>
+                           <div className={item.fav_icon}>
+                              <Fav />
+                           </div>
+                           <span className={item.rate}>{product.rating.rate}</span>
+                        </div>
+ 
+                        <button>Adicionar</button>
+                    </div>
+                </footer>
+            </div>
+
         </li>
     )
 }
